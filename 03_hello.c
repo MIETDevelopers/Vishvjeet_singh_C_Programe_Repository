9dc21e0126ae28216ce8a0ce8cbc99f6e7553ec1
vishvjeet135 <76393836+vishvjeet135@users.noreply.github.com>
2021-04-03 21:42:52 +0530

    Hello World

---
+++ 03_hello.c
@@ -0,0 +1,5 @@
+#include<stdio.h> 	//Pre processive directive to include standard input and output function headerfile.
+int main(){	//Start of main function body.
+	printf("hello world");	//printf  function call to print "hello,world!".	
+	return 0;	//return statement.
+}	//end of main function body
